345ea87d33e0a05187b57b390f7c49806c85c152
Andrew GALAVAN <agalavan@student.unit.ua>
2018-04-09 21:51:17 +0300

    d05 ex05 done

---
+++ d05/ex03/ex03.sql
@@ -0,0 +1,1 @@
+INSERT INTO db_agalavan.ft_table (login, `group`, creation_date) SELECT last_name, 'other', birthdate FROM db_agalavan.user_card WHERE last_name LIKE '%a%' AND LENGTH(last_name) < 9 ORDER BY last_name LIMIT 10;
---
+++ d05/ex05/ex05.sql
@@ -0,0 +1,1 @@
+DELETE FROM db_agalavan.ft_table WHERE id < 6;
